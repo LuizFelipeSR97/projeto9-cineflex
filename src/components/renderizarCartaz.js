03877dacd8ea6191826830565ca15282e7fec23d
LuizFelipeSR97 <luizfsribeiro97@gmail.com>
2022-07-26 22:38:41 -0300

    implementacao de routes

--- src/components/renderizarCartaz.js
+++ src/components/renderizarCartaz.js
@@ -1,9 +1,12 @@
+import Horarios from "./horarios"
+import { Link } from "react-router-dom"
+
 export default function RenderizarCartaz({listaFilmes}){
     return (listaFilmes.map (filme => 
         <div className="cartazFilme">
             <div className="fundoFilme">
                 <div className="imagemFilme">
-                    <img src={filme.posterURL} alt=""/>
+                    <Link to="/sessoes/idFilme"><img src={filme.posterURL} alt="" /></Link>
                 </div>
             </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import ReactDOM from "react-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Home from "./components/home";
 import Horarios from "./components/horarios";
 import "./css/style.css"
 
-export default function App () {
-    return <Horarios />
+function App () {
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sessoes/idFilme" element={<Horarios />} />
+        </Routes>
+    </BrowserRouter>
 }
 
 ReactDOM.render(<App />, document.querySelector(".root"));
